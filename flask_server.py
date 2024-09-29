311b2f003f0414a2c0169118c29815f02f2ec25d
Loldude0 <atajne@umd.edu>
2024-09-29 06:26:37 +0000

    31 lines of imports doesn't particularly excite me

--- flask_server.py
+++ flask_server.py
@@ -10,43 +10,29 @@ import requests
 import os
 from collections import defaultdict
 from recommendation_training.rec import recommend_study
-
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 from graphi import run_rag_agent
 from todo import get_todo
-
 from question import generate_question, check_answer, clear_doubt
-from flask import Flask, jsonify, request
 from uagents import Model
 from uagents.query import query
 from uagents.envelope import Envelope
 import asyncio
-
-# from custom_input_agent import PDFInputResponse
 import json
 import os
-
 from dotenv import load_dotenv
-
-load_dotenv()
-
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-
 from langchain.text_splitter import CharacterTextSplitter
-
 from langchain_community.document_loaders import JSONLoader
 import json
 from pathlib import Path
 from pprint import pprint
 from langchain_openai import OpenAIEmbeddings
-
 from langchain_community.document_loaders import PyPDFLoader
-
 from bson.objectid import ObjectId
-from flask import jsonify, request
 from pymongo.errors import PyMongoError
 
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 embeddings = OpenAIEmbeddings()
 
 app = Flask(__name__)
@@ -93,11 +79,11 @@ def rag_endpoint():
     data = request.json
     if not data or "question" not in data:
         return jsonify({"error": "Missing 'question' in request body"}), 400
-    
+
     result = run_rag_agent(data["question"])
     messages.append(data["question"])
     messages.append(result["answer"])
-    
+
     return jsonify(messages)
 
 
@@ -242,7 +228,7 @@ def api_clear_doubt():
     data = request.json
     if not data:
         return jsonify({"error": "No JSON data provided"}), 400
-    
+
     conversation = data.get("conversation")
     question = data.get("question")
     course = data.get("course")
