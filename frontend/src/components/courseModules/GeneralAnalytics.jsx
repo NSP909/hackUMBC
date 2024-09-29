14d8159f46e1cf1baed9f4b5eb4c1e2a72e0d842
Loldude0 <atajne@umd.edu>
2024-09-29 09:53:02 +0000

    anal lytics

--- frontend/src/components/courseModules/GeneralAnalytics.jsx
+++ frontend/src/components/courseModules/GeneralAnalytics.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+const location = useLocation();
+
+const courses = [
+  { id: 1, name: 'Introduction to Data Science', code: 'CMSC320', icon: BookOpen, description: 'Learn the basics of data science and machine learning' },
+  { id: 2, name: 'Algorithms', code: 'CMSC351', icon: Code, description: 'Learn about algorithms' },
+  { id: 3, name: 'Oral Communication', code: 'COMM107', icon: Globe, description: 'Get better at public speaking' },
+  { id: 4, name: 'Linear Algebra', code: 'MATH240', icon: Database, description: 'Get introduced to the world of Linear Algebra' },
+  { id: 5, name: 'Calculus III', code: 'MATH241', icon: Brain, description: 'Discover the complexities of Calculus III' },
+  { id: 6, name: 'Differential Equations', code: 'MATH246', icon: ChartLine, description: 'See how differential equations are used in the real world' },
+];
+
 function GeneralAnalytics() {
   return (
     <div>GeneralAnalytics</div>
